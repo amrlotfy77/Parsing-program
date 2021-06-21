a89a00246314af48e1b0c7f40bcf9cae499cfb5f
amr <amr_lotfy77@yahoo.com>
2021-06-21 23:37:05 +0200

    add csv parser

--- parser/parser_types/csv_parser.py
+++ parser/parser_types/csv_parser.py
@@ -1,4 +1,37 @@
+import csv
+
 
 
 class CsvParser:
-    pass
+
+    def __init__(self, parser_files):
+        self.parser_file_customer = parser_files[0]
+        self.parser_file_vehicles = parser_files[1]
+
+        self.parsed_content_vehicles = []
+        self.parsed_content = []
+        self.parsing_vehicles()
+        self.parsing_content()
+
+    def parsing_vehicles(self):
+        dict_reader = csv.DictReader(self.parser_file_vehicles)
+        for v_data in dict_reader:
+            self.parsed_content_vehicles.append(v_data)
+
+    def get_vehicles(self, _id):
+        lst = []
+        for i, row in enumerate(self.parsed_content_vehicles):
+            if row.get('owner_id') == _id:
+                del row['owner_id']
+                lst.append(row)
+        return lst
+
+    def parsing_content(self):
+        for line in csv.DictReader(self.parser_file_customer):
+            line['vehicles'] = self.get_vehicles(line['id'])
+            # print(line)
+            self.parsed_content.append(line)
+
+    def get_content(self):
+        return self.parsed_content
+
