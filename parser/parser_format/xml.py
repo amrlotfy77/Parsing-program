0e6b093d1fdd667c2d0ad1851b51b5a923b83770
amr <amr_lotfy77@yahoo.com>
2021-06-21 04:24:36 +0200

    XmlDataFormatter handling required format to save in json file

---
+++ parser/parser_format/xml.py
@@ -0,0 +1,37 @@
+from collections import OrderedDict
+
+
+class XmlDataFormatter:
+
+    @staticmethod
+    def handle_vehicle_data(vehicle_data):
+        formatted_data = []
+        if not vehicle_data['Transaction']['Customer'].get('Units'):
+            return formatted_data
+
+        for vehicle in vehicle_data['Transaction']['Customer']['Units']['Auto']['Vehicle']:
+            print(vehicle)
+            formatted_data.append(
+                {
+                    "id": vehicle['@id'],
+                    "make": vehicle['Make'],
+                    "vin_number": vehicle['VinNumber'],
+                }
+            )
+        return formatted_data
+
+    def format_data(self, data):
+        formatted_data = OrderedDict({
+            "transaction": {
+                "date": data['Transaction']['Date'],
+                "customer": {
+                    "id": data['Transaction']['Customer']['@id'],
+                    "name": data['Transaction']['Customer']['Name'],
+                    "address": data['Transaction']['Customer']['Address'],
+                    "phone": data['Transaction']['Customer']['Phone']
+                },
+                "vehicles": self.handle_vehicle_data(data)
+            }
+        })
+
+        return formatted_data
--- parser/parser_types/xml_parser.py
+++ parser/parser_types/xml_parser.py
@@ -1,10 +1,13 @@
 import xmltodict
 
+from ..parser_format.xml import XmlDataFormatter
+
 
 class XmlParser:
 
     def __init__(self, parser_files):
         self.parser_file = parser_files[0]
+        self.parser_formatter = XmlDataFormatter()
         self.parsed_content = None
         self.parsing_content()
 
@@ -12,4 +15,4 @@ class XmlParser:
         self.parsed_content = xmltodict.parse(self.parser_file.read())
 
     def get_content(self):
-        return self.parsed_content['Insurance']
+        return self.parser_formatter.format_data(self.parsed_content['Insurance'])
