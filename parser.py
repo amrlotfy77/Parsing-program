3382aa434895c5236152b4ed41781733b66877e4
amr <amr_lotfy77@yahoo.com>
2021-06-22 19:46:27 +0200

    initialize database then save parsing data

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ import sys
 
 from parser.parser_factory import TypeFactory
 from operation.file_operation import FileOperation
+from operation.database_operation import Database
 
 
 def main():
@@ -15,6 +16,9 @@ def main():
     parser_files_objects = FileOperation(files)
     parsed_content = parser(parser_files_objects.get_content())
     parser_files_objects.save(parsed_content.get_content(), format_)
+
+    mongo_database = Database()
+    mongo_database.insert(parsed_content.get_content(), format_)
 
 
 if __name__ == '__main__':
