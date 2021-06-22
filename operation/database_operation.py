4ff48eee589b75ad98fb17fe108fe791ec92d6fa
amr <amr_lotfy77@yahoo.com>
2021-06-22 19:41:24 +0200

    database class for set-up connection and insert data in mongo

--- operation/database_operation.py
+++ operation/database_operation.py
@@ -1,2 +1,20 @@
+import pymongo
+import urllib
+
+
+class Database:
+
+    DATABASE = 'trufla'
+    URL = f'mongodb://trufla_admin:P%40ssw0rd@localhost:27017/'
+
+    def __init__(self):
+        self.conn = pymongo.MongoClient(self.URL)
+        self.db = self.conn[self.DATABASE]
+
+    def insert(self, files_content, collection):
+
+        for f_content in files_content:
+
+            self.db[collection].insert(f_content)
 
 
